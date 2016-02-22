f51b5a2c3185ff922f40d8603e02c3fe6cfbf6f5
PeterLaptop <PeterLaptop@10.16.162.239>
2016-02-22 15:37:39 +0100

    oprettelse af standard klasser

---
+++ CDIO/src/IOperatoerDAO.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public interface IOperatoerDAO {
+	OperatoerDTO getOperatoer(int oprId) throws DALException;
+	List<OperatoerDTO> getOperatoerList() throws DALException;
+	void createOperatoer(OperatoerDTO opr) throws DALException;
+	void updateOperatoer(OperatoerDTO opr) throws DALException;
+	void deleteOperatoer(OperatoerDTO opr) throws DALException;
+}
---
+++ CDIO/src/OperatoerDTO.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OperatoerDTO {
+	int oprId;
+	String oprNavn;
+	String ini;
+	int cpr;
+	char[] password;
+
+public static void main(String[] arg)
+{
+@SuppressWarnings("resource")
+Scanner scanner = new Scanner(System.in);
+System.out.println("Indtast CPR nr.");
+
+int nr = scanner.nextInt();
+
+	
+
+}
+}
