f4ff045c4f9f5d29607f5d68e0c50bc34d485327
Peterpc <Peterpc@Peter>
2016-02-24 16:15:17 +0100

    Oprettet OPID og password exception classes

--- CDIO/src/OpIdException.java
+++ CDIO/src/OpIdException.java
@@ -1,8 +1,12 @@
+public class OpIdException extends Exception {
 
-public class OpIdException extends Exception{
+	private static final long serialVersionUID = 1L;
+	private int oprId;
 
-	public OpIdException() {
-		// TODO Auto-generated constructor stub
+	public OpIdException(int oprId) {
+
+		super("operatør ID er forkert");
+		this.oprId = oprId;
 	}
 
 }
--- CDIO/src/OpPasswordException.java
+++ CDIO/src/OpPasswordException.java
@@ -1,8 +1,16 @@
-
 public class OpPasswordException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String password;
+	
 
-	public OpPasswordException() {
-		// TODO Auto-generated constructor stub
+	public OpPasswordException(String password) {
+	
+		super("Koden overholder ikke reglerne for kodeord");
+		this.password = password;
+		
+		
 	}
-
 }
