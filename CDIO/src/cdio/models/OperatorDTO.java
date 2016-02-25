cc0aaa8100b8335604aaaeed8b69d7cbba5a73fc
Lasse Holm Nielsen <lasse1991@yahoo.dk>
2016-02-25 15:29:22 +0100

    [MOD]: Småt

--- CDIO/src/cdio/models/OperatorDTO.java
+++ CDIO/src/cdio/models/OperatorDTO.java
@@ -1,88 +1,97 @@
 package cdio.models;
 
-public class OperatorDTO {
-	private int oprId;
-	private String oprNavn;
-	private String ini;
-	private int cpr;
-	private String password;
-	private int rank;
+public class OperatorDTO 
+{
+    private int oprId;
+    private String oprNavn;
+    private String ini;
+    private long cpr;
+    private String password;
+    private int rank;
 
-	public OperatorDTO(int opr)
-	{
-		oprId = opr;
-	}
+    public OperatorDTO(int opr)
+    {
+            oprId = opr;
+    }
 
-	public void setIni(String ini)
-	{
-		this.ini = ini;
+    public OperatorDTO(int oprId, String oprNavn, String ini, long cpr, String password, int rank) {
+        this.oprId = oprId;
+        this.oprNavn = oprNavn;
+        this.ini = ini;
+        this.cpr = cpr;
+        this.password = password;
+        this.rank = rank;
+    }
+    
+    public void setIni(String ini)
+    {
+            this.ini = ini;
 
-	}
+    }
 
-	public String getIni()
-	{
-		return this.ini;
+    public String getIni()
+    {
+            return this.ini;
 
-	}
+    }
 
-	public void setRank(int rank)
-	{
-		this.rank = rank;
+    public void setRank(int rank)
+    {
+            this.rank = rank;
 
-	}
+    }
 
-	public int getRank() 
-	{
-		return this.rank;
+    public int getRank() 
+    {
+            return this.rank;
 
-	}
+    }
 
-	public void setoprId(int oprId) 
-	{
-		this.oprId = oprId;
+    public void setoprId(int oprId) 
+    {
+            this.oprId = oprId;
 
-	}
+    }
 
-	public int getoprId() 
-	{
-		return this.oprId;
+    public int getoprId() 
+    {
+            return this.oprId;
 
-	}
+    }
 
-	public void setName(String name)
-	{
-		this.oprNavn = name;
+    public void setName(String name)
+    {
+            this.oprNavn = name;
 
-	}
+    }
 
-	public void setCpr(int cpr) 
-	{
-		this.cpr = cpr;
+    public void setCpr(long cpr) 
+    {
+            this.cpr = cpr;
 
-	}
+    }
 
-	public void setPassword(String password)
-	{
-		this.password = password;
+    public void setPassword(String password)
+    {
+            this.password = password;
 
-	}
+    }
 
-	public String getName()
-	{
-		return this.oprNavn;
+    public String getName()
+    {
+            return this.oprNavn;
 
-	}
+    }
 
-	public int getCpr()
-	{
-		return this.cpr;
+    public long getCpr()
+    {
+            return this.cpr;
 
-	}
+    }
 
-	public String getPassword() 
-	{
-		return this.password;
+    public String getPassword() 
+    {
+            return this.password;
 
-	}
-
+    }
 }
