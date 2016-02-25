4a87a3aec2cdae57364bb2120badbc1720d4f63f
Asger <asger.stage@hotmail.com>
2016-02-25 13:17:56 +0100

    Variabler gjordt private, setini og getini tilføjet og kode formateret

--- CDIO/src/cdio/models/OperatorDTO.java
+++ CDIO/src/cdio/models/OperatorDTO.java
@@ -1,73 +1,88 @@
 package cdio.models;
 
-import java.util.List;
-import java.util.Scanner;
-
 public class OperatorDTO {
-	int oprId;
-	String oprNavn;
-	String ini;
-	int cpr;
-	String password;
-	int rank;
-public OperatorDTO(int opr)
-{
-oprId=opr;	
-}
+	private int oprId;
+	private String oprNavn;
+	private String ini;
+	private int cpr;
+	private String password;
+	private int rank;
 
-public void setRank(int rank)
-{
-	this.rank=rank;
-	
-}
-public int getRank()
-{
-	return this.rank;
-	
-}
-public void setoprId(int oprId)
-{
-	this.oprId=oprId;
-	
-}
-public int getoprId()
-{
-	return this.oprId;
-	
-}
-public void setName(String name)
-{
-	this.oprNavn=name;
-	
-}
-public void setCpr(int cpr)
-{
-	this.cpr=cpr;
-	
-}
+	public OperatorDTO(int opr)
+	{
+		oprId = opr;
+	}
 
-public void setPassword(String password)
-{
-	this.password=password;
-	
-}
-public String getName()
-{
-	return this.oprNavn;
-	
-}
-public int getCpr()
-{
-	return this.cpr;
-	
-}
+	public void setIni(String ini)
+	{
+		this.ini = ini;
 
-public String getPassword()
-{
-	return this.password;
-	
-}
+	}
 
+	public String getIni()
+	{
+		return this.ini;
 
+	}
+
+	public void setRank(int rank)
+	{
+		this.rank = rank;
+
+	}
+
+	public int getRank() 
+	{
+		return this.rank;
+
+	}
+
+	public void setoprId(int oprId) 
+	{
+		this.oprId = oprId;
+
+	}
+
+	public int getoprId() 
+	{
+		return this.oprId;
+
+	}
+
+	public void setName(String name)
+	{
+		this.oprNavn = name;
+
+	}
+
+	public void setCpr(int cpr) 
+	{
+		this.cpr = cpr;
+
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+
+	}
+
+	public String getName()
+	{
+		return this.oprNavn;
+
+	}
+
+	public int getCpr()
+	{
+		return this.cpr;
+
+	}
+
+	public String getPassword() 
+	{
+		return this.password;
+
+	}
 
 }
