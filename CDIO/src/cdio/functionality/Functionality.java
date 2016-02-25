5578cbab95afe01d50f0a24134cb9f13d583fd93
mariamsaidi <mariamsaidi>
2016-02-25 13:23:10 +0100

    Jeg har lavet en metode til at tilføje og slette en opr også har jeg lavet andre småting som jeg ikke er færdig med

--- CDIO/src/cdio/functionality/Functionality.java
+++ CDIO/src/cdio/functionality/Functionality.java
@@ -3,52 +3,76 @@ package cdio.functionality;
 import cdio.models.OperatorDTO;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Functionality implements IFunctionality {
+
+	List<OperatoerDTO> operatoerer= new ArrayList <OperatoerDTO>();
 
 	public Functionality() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void createOpr(OperatorDTO opr) {
+	public void createOpr(String oprNavn, int cpr) { 
 		// TODO Auto-generated method stub
-		
+		OperatoerDTO opr = new OperatoerDTO(oprId, oprNavn, ini, cpr, password); // Ny operator oprettes (der skal laves nogle metoder til at autogenerer oprId og ini )
+		operatoerer.add(opr); // operator tilføjes en liste over operatører 
+		return; // Her skal der returneres et oprId eller en ini-kode når operatøren er oprettet
 	}
 
 	@Override
-	public void deleteOpr(OperatorDTO opr) {
+	public boolean deleteOpr (int oprId) {
 		// TODO Auto-generated method stub
-		
+		for(OperatoerDTO oprDTO: operatoerer){
+			if(oprDTO.oprId==oprId){
+				operatoerer.remove(oprDTO);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
-	public void updateOpr(OperatorDTO opr) {
+	public void updateOpr(int oprId) {
 		// TODO Auto-generated method stub
-		
+		for (){
+			if (valg==1)//hvis det er navnet der skal ændres
+				operatoerer.get(navn).setNavn();
+			else if (valg==2)//hvis det er cpr der skal ændres
+				operatoerer.get(cpr).setCpr();
+			else if (valg==2)//hvis det er opr status der skal ændres
+				operatoerer.get(oprStatus).setStatus();
+		}
+		// mulige ændringer
+		// 1. Navn
+		// 2. Cpr
+		// 3. Status ( om det er en alm opr el admin )
+
 	}
 
 	@Override
-	public OperatorDTO readOpr(OperatorDTO opr) {
+	public boolean changePass(int oprId) {
 		// TODO Auto-generated method stub
-		return null;
+		for(OperatoerDTO oprDTO: operatoerer){
+			if(oprDTO.oprId==oprId){
+				operatoerer.get(password).setPassword();
+				return true;
+			}
+		}
+		return false;
+	}
+	double measure(int tara, int weight) {
+		// TODO Auto-generated method stub
+		return weight-tara;
+
 	}
 
 	@Override
-	public void changePass(String newPassword) {
+	public void login() {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public double measure(double tara, double netto) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void login(String password) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
--- CDIO/src/cdio/functionality/IFunctionality.java
+++ CDIO/src/cdio/functionality/IFunctionality.java
@@ -4,12 +4,15 @@ import cdio.models.OperatorDTO;
 
 
 public interface IFunctionality {
-void createOpr(OperatorDTO opr);
-void deleteOpr(OperatorDTO opr);
-void updateOpr(OperatorDTO opr);
-OperatorDTO readOpr(OperatorDTO opr); //er det korrekt forst�et at den skal returnere et operat�r objekt??
-void changePass(String newPassword);
-double measure(double tara, double netto);
-void login(String password);
-	
+	public void createOpr(String oprNavn, int cpr);
+	public boolean deleteOpr(int oprId);
+	public void updateOpr(int oprId);
+	OperatorDTO readOpr(OperatorDTO opr); //er det korrekt forst�et at den skal returnere et operat�r objekt??
+	public boolean changePass(int oprId);
+	double measure(double tara, double netto);
+	void login(String password);
+
 }
+
+
+
