f43746f99dce4328872d1e3d63338f991efbb884
Lasse Holm Nielsen <lasse1991@yahoo.dk>
2016-02-25 14:35:52 +0100

    [MOD]: Ryttet op i main klassen

--- CDIO/src/cdio/Main.java
+++ CDIO/src/cdio/Main.java
@@ -1,25 +1,14 @@
 package cdio;
-
-import cdio.models.OperatorDTO;
 import cdio.exceptions.DALException;
 import cdio.data.OperatorDAO;
 import cdio.functionality.Functionality;
 import cdio.view.View;
 import cdio.view.IView;
 
-
 public class Main 
 {
     public static void main(String[] args) throws DALException {
-       
-    	OperatorDAO ops =new OperatorDAO();
-    	OperatorDTO op=new OperatorDTO(11);
-    	ops.createOperatoer(op);
-    	
-        IView view = new View(new Functionality(new OperatorDAO()));
+        final IView view = new View(new Functionality(new OperatorDAO()));
         view.dialog();
-    	
-    	
-    	System.out.println(ops.getOperator(11)); 
     }
 }	
