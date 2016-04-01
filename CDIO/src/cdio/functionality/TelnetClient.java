f323c3d36a64a7ab053525a5dbf94287da63e2cc
Lasse Holm Nielsen S123954 <Lasse H Nielsen@dtu>
2016-04-01 14:28:56 +0200

    [ADD]: Sikker lukning af forbindelsen til serveren. [MOD]: Kodeoprydning.

--- CDIO/src/cdio/functionality/TelnetClient.java
+++ CDIO/src/cdio/functionality/TelnetClient.java
@@ -1,42 +1,78 @@
 package cdio.functionality;
 import java.io.*;
-import java.net.*;	
-public class TelnetClient {
-private String host;
-Socket s = new Socket();
-PrintWriter s_out =null;
-BufferedReader s_in = null;
+import java.net.*;
 
-	public TelnetClient(String host) {
-		this.host=host;
-		try {
-			s.connect(new InetSocketAddress("localhost",8000));
-			System.out.println("Connected");
-			s_out = new PrintWriter(s.getOutputStream(), true);
-			s_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		} catch (IOException e) {
-			
-			e.printStackTrace();
-		}
-		
-	}
-
-	public void getData(String msg){
-	
-		
-	s_out.println(msg);
-	System.out.println("Message sent");
-	String response;
-	try{
-		//while((response= s_in.readLine()) !=null){
-			System.out.println(s_in.readLine());
-		//System.out.println(response);
-		//}
-		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-	}
-	
-
+public class TelnetClient
+{
+    private final Socket s = new Socket();
+    private final String host;
+    private final int port;
+    private PrintWriter s_out;
+    private BufferedReader s_in;
+    
+    protected TelnetClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+    
+    protected void connect() throws IOException {
+        try {
+            s.connect(new InetSocketAddress(host, port));
+            System.out.println("Connected to " + host + " on port " + port);
+            
+            s_out = new PrintWriter(s.getOutputStream(), true);
+            s_in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException("Could not connect to " + host + " on port " + port);
+        }
+    }
+    
+    protected String getData(String msg) throws IOException {
+        String response = null;
+        s_out.println(msg);
+        System.out.println("Request:\t\"" + msg + '\"');
+        
+        try {
+//            while((response= s_in.readLine()) != null) {
+            response = s_in.readLine();
+            System.out.println("Response:\t\"" + response + '\"');
+//            }
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+            System.err.println("Closing connection");
+            close();
+        }
+        
+        return response;
+    }
+    
+    protected void close() throws IOException {        
+        try {
+            if(s_out != null) {
+                s_out.flush();
+                s_out.close();
+            }
+            if(s_in != null)
+                s_in.close();
+        } 
+        catch (IOException e) {
+            System.out.println("Could not close reader or writer: " + e.getMessage());
+            e.printStackTrace();
+            throw new IOException("Could not close reader or writer");
+        }
+        finally {
+            if(s != null)
+                try {
+                    s.close();
+                } 
+                catch (IOException e) {
+                    System.out.println("Could not close connection: " + e.getMessage());
+                    e.printStackTrace();
+                    throw new IOException("Could not close connection");
+                }
+        }
+    }
+}
