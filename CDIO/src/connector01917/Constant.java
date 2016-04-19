8a32fa439d32f9323570814357ec233554a06be0
Asger <asger.stage@hotmail.com>
2016-04-19 08:50:22 +0200

    [ADD]Database klasser og pakker tilføjet. Det er lidt rodet..

---
+++ CDIO/src/connector01917/Constant.java
@@ -0,0 +1,16 @@
+package connector01917;
+
+
+// erstat konstanterne nedenfor
+
+public abstract class Constant
+{
+	public static final String
+		server					= "127.0.0.1",  // database-serveren
+		database				=  "raavarer",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "s133284", // dit brugernavn = dit studienummer 
+		password				= "1234"; // dit password som du har valgt til din database
+	
+	public static final int
+		port					= 3306;
+}
