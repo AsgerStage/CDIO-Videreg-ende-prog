e7d728b974bc1a6696f2a56f3ab49b5451b88cf5
psshs <asger.stage@hotmail.com>
2016-05-07 00:32:15 +0200

    [Remove]Fjernet gamle WeightWeb Projekt

--- WeightWeb/src/cdio3/client/service/ClientInterface.java
+++
@@ -1,9 +0,0 @@
-package cdio3.client.service;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ClientInterface {
-	void asd(String name);
-	
-	void asd2 (int num1,int num2);
-}
