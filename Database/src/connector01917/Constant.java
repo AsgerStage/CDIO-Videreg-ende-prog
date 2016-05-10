59d036b33fcd76bf2dca3b1c749b0ed8b7d2f529
Lasse H Nielsen <LasseHNielsen@s123954-DTU>
2016-05-10 10:45:39 +0200

    [FIX]: Indsat database info i Constants klassen.

--- Database/src/connector01917/Constant.java
+++ Database/src/connector01917/Constant.java
@@ -6,10 +6,10 @@ package connector01917;
 public abstract class Constant
 {
 	public static final String
-		server					= "sql-lab1.cc.dtu.dk",  // database-serveren
-		database				=  "sXXXXXX",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "sXXXXXX", // dit brugernavn = dit studienummer 
-		password				= ""; // dit password som du har valgt til din database
+		server					= "nasie.diskstation.me",  // database-serveren
+		database				=  "DTU",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "DTU", // dit brugernavn = dit studienummer 
+		password				= "Gruppe16"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
