9e44a8b789599fdf0d8e1aeb1f1a25f298cab64b
Lasse H Nielsen <LasseHNielsen@s123954-DTU>
2016-06-08 17:45:30 +0200

    [ADD]: Lavet rediger og opret popup vindue til råvarer.

---
+++ RPCExample/src/edu/example/client/gui/raavare/RaavareInfoPopup.java
@@ -0,0 +1,89 @@
+package edu.example.client.gui.raavare;
+
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import edu.example.client.models.RaavareDTO;
+
+public class RaavareInfoPopup extends DialogBox 
+{	
+	private TextBox nameBox = new TextBox();
+	private TextBox leverandoerBox = new TextBox();
+	
+	private Button cancel = new Button("Anuller");
+	private Button execute = new Button();
+	
+	private boolean isCreate = false;
+	
+	public RaavareInfoPopup(RaavareDTO raavare) {
+		super();
+		VerticalPanel mainPanel = new VerticalPanel();
+		
+		HTML title = new HTML();
+		
+		if(raavare == null) {
+			isCreate = true;
+			
+			this.setTitle("Opret Raavare");
+			this.setText("Opret Raavare");
+			
+			title.setHTML("Opret R&aring;vare");
+			execute.setText("Opret");
+		}
+		else {
+			this.setTitle("Rediger Raavare");
+			this.setText("Rediger Raavare");
+			
+			title.setHTML("Rediger R&aring;vare");
+			nameBox.setText(raavare.getRaavareNavn());
+			leverandoerBox.setText(raavare.getLeverandoer());
+			execute.setText("Gem");
+		}
+		
+		HTML labelNavn = new HTML("R&aring;vare Navn");
+		HTML labelLeverandoer = new HTML("Leverand&oslash;r");
+		
+		FlexTable flxTable = new FlexTable();
+        flxTable.setWidget(0, 0, labelNavn);
+        flxTable.setWidget(0, 1, nameBox);
+        flxTable.setWidget(1, 0, labelLeverandoer);
+        flxTable.setWidget(1, 1, leverandoerBox);
+		
+		HorizontalPanel buttonPanel = new HorizontalPanel();
+        buttonPanel.setSpacing(10);
+        buttonPanel.add(execute);
+        buttonPanel.add(cancel);
+        
+        mainPanel.add(title);
+        mainPanel.add(flxTable);
+        mainPanel.add(buttonPanel);
+        
+        this.setWidget(mainPanel);
+	}
+	
+	protected String getRaavareName() {
+		return nameBox.getText();
+	}
+	
+	protected String getRaavareLeverandoer() {
+		return leverandoerBox.getText();
+	}
+	
+	protected boolean isCreate() {
+		return isCreate;
+	}
+	
+	protected void setExecuteClickHandler(ClickHandler clickHandler) {
+        execute.addClickHandler(clickHandler);
+	}
+	
+	protected void setCancelClickHandler(ClickHandler clickHandler) {
+        cancel.addClickHandler(clickHandler);
+	}
+}
---
+++ RPCExample/src/edu/example/client/gui/raavare/RaavarePanel.java
@@ -0,0 +1,246 @@
+package edu.example.client.gui.raavare;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HasAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.PushButton;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import edu.example.client.gui.MenuWidget;
+import edu.example.client.models.OperatorDTO;
+import edu.example.client.models.RaavareDTO;
+import edu.example.client.service.ExampleServiceClientImpl;
+
+public class RaavarePanel extends Composite
+{
+	public MenuWidget parent;
+	private ExampleServiceClientImpl serverComm;
+	private VerticalPanel mainPanel = new VerticalPanel();
+	
+	private final String searchBoxDefaultText = "Soeg efter raavare";
+	private final TextBox searchBox = new TextBox();
+	
+	private final Grid tableList = new Grid(1, 4);
+	private List<RaavareDTO> raavareList = null;
+	private List<RaavareDTO> dispRaavareList = null;
+	
+	public RaavarePanel(MenuWidget parent, ExampleServiceClientImpl serverComm) {
+		this.parent = parent;
+		this.serverComm = serverComm;
+		
+		mainPanel.setSize("100%", "100%");
+		initWidget(mainPanel);
+		init();
+		
+		this.serverComm.getRaavareList();
+	}
+	
+	private void init() {
+		//Top panel
+		HorizontalPanel topPanel = new HorizontalPanel();
+		
+		HTML pageTitle = new HTML("R&aring;varer");
+		pageTitle.addStyleName("h1");
+		
+		searchBox.setText(searchBoxDefaultText);
+		
+		Button buttonSearch = new Button();
+		buttonSearch.setHTML("S&oslash;g");
+		buttonSearch.addClickHandler(new ClickHandlerSearch());
+		
+		Button buttonUpdate = new Button("Opdater");
+		buttonUpdate.addClickHandler(new ClickHandlerUpdate());
+		
+		//Table
+		tableList.setHTML(0, 0, "ID");
+		tableList.setHTML(0, 1, "R&aring;vare Navn");
+		tableList.setHTML(0, 2, "Levenrad&oslash;r");
+		tableList.setHTML(0, 3, "Handling");
+		tableList.setBorderWidth(1);
+		tableList.setCellPadding(10);
+		tableList.setWidth("100%");
+		
+		//Buttom panel
+		HorizontalPanel buttonPanel = new HorizontalPanel();
+		buttonPanel.setWidth("100%");
+		buttonPanel.setHorizontalAlignment(HasAlignment.ALIGN_RIGHT);
+		buttonPanel.addStyleName("paddedVerticalPanel");
+		
+		Button createButtom = new Button();		
+		createButtom.setHTML("Opret R&aring;vare");
+		createButtom.setStyleName("button");
+		createButtom.addClickHandler(new ClickHandlerCreate());
+		
+		//Add widgets
+		topPanel.add(pageTitle);
+		topPanel.add(searchBox);
+		topPanel.add(buttonSearch);
+		topPanel.add(buttonUpdate);
+		
+		buttonPanel.add(createButtom);
+		
+		mainPanel.add(topPanel);
+		mainPanel.add(tableList);
+		mainPanel.add(buttonPanel);
+	}
+	
+	public void updateTable(List<RaavareDTO> raavarer) {
+		raavareList = raavarer;
+		update(raavareList);
+	}
+	
+	private void update(List<RaavareDTO> raavarer) {
+		dispRaavareList = raavarer;
+		clearTable(raavarer.size() + 1);
+		
+		for (int i = 0; i < raavarer.size(); i++) {
+			addRaavareToTable(i + 1, raavarer.get(i));
+		}
+	}
+	
+	private void addRaavareToTable(int rowIndex, RaavareDTO raavare) {
+		tableList.setText(rowIndex, 0, "" + raavare.getRaavareID());
+		tableList.setText(rowIndex, 1, raavare.getRaavareNavn());
+		tableList.setText(rowIndex, 2, raavare.getLeverandoer());
+		
+		HorizontalPanel ButtonPanel = new HorizontalPanel();
+		
+		PushButton editButton = new PushButton(new Image("Billeder/edit-icon.png"));
+		editButton.addClickHandler(new ClickHandlerEdit());
+		editButton.setTitle("Rediger");
+		ButtonPanel.add(editButton);
+		
+		PushButton deleteButton = new PushButton(new Image("Billeder/trash-icon.png"));
+		deleteButton.addClickHandler(new ClickHandlerDelete());
+		deleteButton.setTitle("Slet");
+		ButtonPanel.add(deleteButton);
+		
+		tableList.setWidget(rowIndex, 3, ButtonPanel);
+	}
+	
+	private void clearTable(int size) {
+		tableList.resize(size, 4);
+	}
+	
+	private class ClickHandlerSearch implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event) {
+			String searchText = searchBox.getText();
+			
+			if(raavareList != null) {
+				ArrayList<RaavareDTO> result = new ArrayList<>();
+				
+				try {
+					int searhID = Integer.parseInt(searchText);
+					
+					for (RaavareDTO raavare : raavareList) {
+						if(raavare.getRaavareID() == searhID) {
+							result.add(raavare);
+							break;
+						}
+					}
+				}
+				catch(NumberFormatException e) {
+					for (RaavareDTO raavare : raavareList) {
+						if(raavare.getRaavareNavn().contains(searchText) || raavare.getLeverandoer().contains(searchText)) {
+							result.add(raavare);
+						}
+					}
+				}
+				finally {
+					if(result != null)
+						update(result);
+				}
+			}
+			
+			
+		}
+	}
+	
+	private class ClickHandlerUpdate implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event) {
+			serverComm.getRaavareList();
+		}
+	}
+	
+	private class ClickHandlerEdit implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event) {
+			RaavareDTO raavare = dispRaavareList.get(tableList.getCellForEvent(event).getRowIndex() - 1);
+			
+			RaavareInfoPopup editPopup = new RaavareInfoPopup(raavare);
+			editPopup.setExecuteClickHandler(new PopupHandlerExecute(editPopup));
+			editPopup.setCancelClickHandler(new PopupHandlerCancel(editPopup));
+			editPopup.show();
+		}
+	}
+	
+	private class ClickHandlerCreate implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event) {
+			RaavareInfoPopup createPopup = new RaavareInfoPopup(null);
+			createPopup.setExecuteClickHandler(new PopupHandlerExecute(createPopup));
+			createPopup.setCancelClickHandler(new PopupHandlerCancel(createPopup));
+			createPopup.show();
+		}
+	}
+	
+	private class ClickHandlerDelete implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event) {
+			
+		}
+	}
+	
+	private class PopupHandlerExecute implements ClickHandler 
+	{
+		private RaavareInfoPopup popup;
+		
+		protected PopupHandlerExecute(RaavareInfoPopup popup) {
+			this.popup = popup;
+		}
+
+		@Override
+		public void onClick(ClickEvent event) {
+			String raavareName = popup.getRaavareName();
+			String leverandør = popup.getRaavareLeverandoer();
+			
+			if(popup.isCreate()) {
+				
+			}
+			else {
+				
+			}
+		}
+	}
+	
+	private class PopupHandlerCancel implements ClickHandler 
+	{
+		private RaavareInfoPopup popup;
+		
+		protected PopupHandlerCancel(RaavareInfoPopup popup) {
+			this.popup = popup;
+		}
+
+		@Override
+		public void onClick(ClickEvent event) {
+			popup.hide();
+		}
+	}
+}
