e683b70fafeda13695e9160861ff7b5628cbe1d1
Lasse Holm Nielsen <lasse1991@yahoo.dk>
2016-06-13 12:02:04 +0200

    bla

--- RPCExample/src/edu/example/server/database/ReceptkompDAO.java
+++ RPCExample/src/edu/example/server/database/ReceptkompDAO.java
@@ -9,7 +9,7 @@ public interface ReceptkompDAO {
 	ReceptkompDTO getReceptkomp(int receptID, int raavareID) throws DALException;
 	List<ReceptkompDTO> getReceptkompList() throws DALException;
 	List<ReceptkompDTO> getReceptkompListByReceptID(int receptId) throws DALException;
-	int createReceptkomp(ReceptkompDTO receptkomp) throws DALException;
+	int createReceptkomp(ReceptkompDTO receptkomp) throws DALException; 
 	int updateReceptkomp(ReceptkompDTO receptkomp) throws DALException;
 	int deleteReceptkomp(int receptID, int raavareID) throws DALException;
 }
